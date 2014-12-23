5bd8cfbdcadb789d3ef061c08a3b48f1c95efa01
spuds <spuds@spudsdesign.com>
2014-12-23 14:57:54 -0600

    ! initial loading

---
+++ languages/Settings.english.php
@@ -0,0 +1,6 @@
+<?php
+
+global $settings;
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.png';
+$txt['theme_description'] = 'A framework theme. Lattice by <a href="http://www.spudsdesign.com">Spuds</a>';
